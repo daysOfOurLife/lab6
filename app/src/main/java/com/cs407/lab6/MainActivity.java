d8ef57b8b12c59a6c3885b7991c759ab0d13f7b0
Victor Nguyen <91512538+daysOfOurLife@users.noreply.github.com>
2023-11-02 22:10:37 -0500

    Completing Milestone 1

--- app/src/main/java/com/cs407/lab6/MainActivity.java
+++ app/src/main/java/com/cs407/lab6/MainActivity.java
@@ -5,9 +5,16 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends FragmentActivity {
+
+    // Bascom Hall Coordinates - 43.07577322416704, -89.4040052983958
+    private final LatLng mDestinationLatLng = new LatLng(43.07577322416704, -89.4040052983958);
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,14 @@ public class MainActivity extends FragmentActivity {
         setContentView(R.layout.activity_main);
         SupportMapFragment mapFragment =(SupportMapFragment)
                 getSupportFragmentManager().findFragmentById(R.id.fragment_map);
+
+        mapFragment.getMapAsync(googleMap -> {
+            mMap = googleMap;
+
+            // Code to display marker
+            googleMap.addMarker(new MarkerOptions()
+                    .position(mDestinationLatLng)
+                    .title("Destination"));
+        });
     }
 }
